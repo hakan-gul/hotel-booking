338441d504b6c099e01896012c36bb913c2f25f6
Hakan GÜL <9hakangl@gmail.com>
2024-11-22 23:25:44 +0300

    slider-pocketbase-settings

--- app/(routes)/_components/Hero.tsx
+++ app/(routes)/_components/Hero.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {
     Carousel,
     CarouselContent,
@@ -9,13 +9,51 @@ import {
   } from "@/components/ui/carousel"
 
   import Autoplay from "embla-carousel-autoplay"
-import { SliderImage } from '@/constans'
+import { apiImagesUrl } from '@/constans'
 import Image from 'next/image'
 import ImageWaves from '@/components/ImageWaves'
 import HeroForm from './HeroForm'
+import { get } from 'http'
+import { getSlider } from '@/actions/getSlider'
+import { RecordModel } from 'pocketbase'
+import { Skeleton } from '@/components/ui/skeleton'
 
 
 const Hero = () => {
+
+  
+  const [SliderImages, setSliderImages] = useState<RecordModel[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() =>{
+      async function fetchSlider(){
+      try {
+        const images = await getSlider();
+        setSliderImages(images);
+        console.log('Slider Images:', images)
+
+      } catch (error) {
+        console.log('Error getting slider images:', error)
+      }
+      finally{
+        setIsLoading(false);
+      }
+    }
+    fetchSlider();
+  },[])
+
+  if(isLoading){
+    return(
+      <div className='relative h-[32rem]'>
+        <div className='h-[32rem] lg:h-[44rem] w-full'>
+          <Skeleton  className='h-full w-full bg-slate-600'/>
+        </div>
+        <HeroForm />
+
+      </div>
+    )
+    
+  }
   return (
     <div className='relative h-[32rem]'>
         <Carousel  
@@ -31,11 +69,11 @@ const Hero = () => {
         }}
        >
             <CarouselContent>
-               {SliderImage.map((image, index)=>(
+               {SliderImages.map((data, index)=>(
                     <CarouselItem key={index}>
                           <Image 
-                          src={image.href} 
-                          alt={image.alt} 
+                          src={`${apiImagesUrl}/${data.collectionId}/${data.id}/${data.image}`} 
+                          alt={data.alt} 
                           width={1920} 
                           height={1080}
                           className='h-[32rem] lg:h-[44rem] w-full object-cover brightness-75'
--- constans/index.ts
+++ constans/index.ts
@@ -1,3 +1,5 @@
+export const apiImagesUrl = 'https://hakngul.pockethost.io/api/files/'
+
 export const NavMenu = [
     {
         title:"Home",
@@ -17,20 +19,20 @@ export const NavMenu = [
     }
 ]
 
-export const SliderImage = [
-    {
-        id:1,
-        href: '/slider/1.jpg',
-        alt: 'slider 1',
-    },
-    {
-        id:2,
-        href: '/slider/2.jpg',
-        alt: 'slider 2',
-    },
-    {
-        id:3,
-        href: '/slider/3.jpg',
-        alt: 'slider 3',
-    }
-]
+// export const SliderImage = [
+//     {
+//         id:1,
+//         href: '/slider/1.jpg',
+//         alt: 'slider 1',
+//     },
+//     {
+//         id:2,
+//         href: '/slider/2.jpg',
+//         alt: 'slider 2',
+//     },
+//     {
+//         id:3,
+//         href: '/slider/3.jpg',
+//         alt: 'slider 3',
+//     }
+// ]
